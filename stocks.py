9e338a2bc6bb6e0da906f9af4b489f461d0c9a29
caglar togan <155642133+CaglarCode@users.noreply.github.com>
2025-07-03 06:56:58 -0400

    Update stocks.py

--- stocks.py
+++ stocks.py
@@ -3,7 +3,7 @@ import yfinance as yf
 def fetch_stock_data(symbol, company, sector):
     try:
         ticker = yf.Ticker(symbol)
-        fast = ticker.fast_info
+        info = ticker.info
 
         return {
             'symbol': symbol,
@@ -14,8 +14,8 @@ def fetch_stock_data(symbol, company, sector):
             '52w_low': info.get('fiftyTwoWeekLow', 'N/A'),
             'change_percent': info.get('regularMarketChangePercent', 'N/A'),
             'dividend': info.get('dividendRate', 'N/A'),
-            'eps': info.get('epsTrailingTwelveMonths', 'N/A'),
-            'pe_ratio': info.get('trailingPE', 'N/A')
+            'eps': info.get( 'epsTrailingTwelveMonths','N/A'),
+            'pe_ratio': info.get( 'trailingPE','N/A')
         }
     except Exception as e:
         return {
