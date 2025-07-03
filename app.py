6395e9c6399d7391dbcfa0ffd3e5260e7d002e51
caglar togan <155642133+CaglarCode@users.noreply.github.com>
2025-07-03 07:54:17 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,22 @@
 from flask import Flask, render_template
 import sqlite3
-from stocks import fetch_stock_data
 
 app = Flask(__name__)
+DATABASE = 'stocks.db'
 
-DB_PATH = 'stocks.db'
+def get_stock_data():
+    conn = sqlite3.connect(DATABASE)
+    conn.row_factory = sqlite3.Row
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM stocks")
+    data = cursor.fetchall()
+    conn.close()
+    return data
 
 @app.route('/')
 def index():
-   
-    conn = sqlite3.connect(DB_PATH)
-    cursor = conn.cursor()
-    cursor.execute("SELECT symbol, company, sector FROM stocks")
-    stocks = cursor.fetchall()
-    conn.close()
-
-    
-    stock_data = [fetch_stock_data(symbol, company, sector) for symbol, company, sector in stocks]
-
-    return render_template('index.html', stocks=stock_data)
+    stocks = get_stock_data()
+    return render_template('index.html', stocks=stocks)
 
 if __name__ == '__main__':
     app.run(debug=True)
