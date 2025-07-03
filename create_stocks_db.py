f3082d6d85d6bd2dbe87f8c6aff42aa00c138d02
caglar togan <155642133+CaglarCode@users.noreply.github.com>
2025-07-03 05:31:01 -0400

    Update create_stocks_db.py

--- create_stocks_db.py
+++ create_stocks_db.py
@@ -1,15 +1,13 @@
-# create_stocks_db.py
 import sqlite3
 import pandas as pd
 
-# Excel dosyasını oku
 df = pd.read_excel("Stock Tracker.xlsx", sheet_name="Sheet1")
 
-# SQLite veritabanını oluştur
+
 conn = sqlite3.connect("stocks.db")
 cursor = conn.cursor()
 
-# Tabloyu oluştur
+
 cursor.execute("DROP TABLE IF EXISTS stocks")
 cursor.execute("""
 CREATE TABLE stocks (
@@ -20,7 +18,7 @@ CREATE TABLE stocks (
 )
 """)
 
-# Gerekli verileri al ve ekle
+
 stock_data = df[['Symbol', 'Company', 'Sector']].dropna().values.tolist()
 cursor.executemany("INSERT INTO stocks (symbol, company, sector) VALUES (?, ?, ?)", stock_data)
 
